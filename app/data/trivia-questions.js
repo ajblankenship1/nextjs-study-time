04141d2e56e7e13b5fdbbf3348d01a9850a64daa
AJ <ajblankenship1@gmail.com>
2024-04-05 07:51:04 +0000

    second commit

---
+++ app/data/trivia-questions.js
@@ -0,0 +1,41 @@
+export default [
+    {   id:0,
+        question: "Start Study Time!",
+        answer:"Give It Your Best Shot!"
+    },
+    {
+        id:1,
+        question:"What occasion corresponds to the longest day of the year?",
+        answer: "The summer solstice"
+    },
+    {
+        id:2,
+        question: "What is the distance from earth to the sun?",
+        answer: "93 million miles"
+    },
+    {
+        id:3,
+        question: "What sport was featured on the first curved U.S. coin in 2014?",
+        answer: "Baseball"
+    },
+    {
+        id:4,
+        question: "Which country is the largest in the world?",
+        answer: "Russia"
+    },
+    {
+        id:5,
+        question: "M&M’S Fruit Chews would eventually become what popular candy?",
+        answer: "Starburst"
+    },
+    {
+        id:6,
+        question: "According to Guinness World Records, what's the best-selling book of all time?",
+        answer: "The Bible"
+    },
+    {
+        id:7,
+        question: "What U.S. state is home to Acadia National Park?",
+        answer: "Maine"
+    },
+]
--- app/page.js
+++ app/page.js
@@ -1,43 +1,123 @@
 'use client'
 import {useState} from 'react'
+import triviaQuestions from './data/trivia-questions';
 
 export default function Home() {
 
 const [numberOfCorrectAnswers,setNumberOfCorrectAnswers] = useState(0);
 const [numberOfWrongAnswers, setNumberOfWrongAnswers] = useState(0);
-const [displayQuestion,setDisplayQuestion] = useState("Start Study Time!");
-const [displayAnswer,setDisplayAnswer] = useState("Give It Your Best Shot!")
-    function ScoreKeeper(){
+const [triviaIndex,setTriviaIndex]= useState(0);
+const [answerToggle,setAnswerToggle] = useState("hide");
+const [questionMarkToggle, setQuestionMarkToggle] = useState("display");
+const [displayDirections, setDisplayDirections] = useState("<<< Click Question Box to go to next question!");
+const [addNewCardClass, setAddNewCardClass] = useState("hide");
+
+
+function handleStartOver(){
+    setNumberOfCorrectAnswers(0);
+    setNumberOfWrongAnswers(0);
+    setTriviaIndex(0);
+    setAnswerToggle("hide");
+    setQuestionMarkToggle("display");
+    setDisplayDirections("<<< Click Question Box to go to next question!");
+}
+
+function ScoreKeeper(){
         return (
             <div className='score-container'>
                 <h3>Correct: {numberOfCorrectAnswers}</h3>
                 <h3>Wrong: {numberOfWrongAnswers}</h3>
-                <button>ADD NEW FLASHCARD</button>
+                <button onClick={handleAddNewFlashcard}>ADD NEW FLASHCARD</button>
+            </div>
+        )
+    }
+    
+    function handleDisplayQuestion(){
+        setTriviaIndex(triviaIndex +1);
+        setAnswerToggle('hide');
+        setQuestionMarkToggle("display");
+        setDisplayDirections("Click Answer Box to reveal the answer! >>> ");
+    }
+
+    function handleDisplayAnswer(){
+        setAnswerToggle('display-answer');
+        setQuestionMarkToggle("hide");
+        setDisplayDirections("How'd you do?? Click one of the buttons below!")
+    }
+
+    function handleAddNewFlashcard(){
+        setAddNewCardClass("display-form");
+    }
+
+    function AddNewCardBox(){
+        return (
+            <div className={addNewCardClass}>
+                <form>
+                    <input type="text" placeholder='Write Question Here'/>
+                    <input type="text" placeholder='Write Answer Here'/>
+                    <button>Submit</button>
+                </form>
             </div>
         )
     }
 
-    function NoteCard(){
+    function NoteCard(props){
+       const data = props.data;
         return(
             <div className='cards-container'>
-                <div className='question-card'>
-                    <p>{displayQuestion}</p>
+                <div className='question-card' onClick={handleDisplayQuestion}>
+                    <h4>Question</h4>
+                    <p>{data.question}</p>
                 </div>
-                <div className='answer-card'>
-                    <p>{displayAnswer}</p>
+                <div className="directions-box">
+                    <h3>{displayDirections}</h3>
+                </div>
+                <div className='answer-card' onClick={handleDisplayAnswer}>
+                <h4>Answer</h4>
+                    <p className={questionMarkToggle}>???</p>
+                    <p className={answerToggle}>{data.answer}</p>
                 </div>
             </div>
             
         )
     }
 
+    function RightOrWrongBox(){
+        return(
+           <div>
+                <div className='right-or-wrong-box'>
+                    <button onClick={handleRight} className='right-button'>Got it right!</button>
+                    <button onClick={handleWrong} className='wrong-button'>Got it wrong...</button>
+                </div>
+                <div className="start-over-button">
+                <button  onClick={handleStartOver}>Start Over</button>
+                </div>
 
+            </div>
+            
+          
+        )
+    }
+
+    function handleRight(){
+        setNumberOfCorrectAnswers(numberOfCorrectAnswers +1);
+        setDisplayDirections("<<< Click Question Box to go to next question!");
+    }
+
+    function handleWrong(){
+        setNumberOfWrongAnswers(numberOfWrongAnswers +1);
+        setDisplayDirections("<<< Click Question Box to go to next question!");
+    }
+
+   
     return (
         <main>
             <h1> Study Time</h1>
             <ScoreKeeper/>
-            <NoteCard/>
-
+            <AddNewCardBox/>
+            <NoteCard data={triviaQuestions[triviaIndex]}/>
+            <RightOrWrongBox/>
+            
         </main>
     );
 }
